4531f6a8978a3bf299db58a6309a06b87f3e1096
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-06-09 16:36:28 +0200

    Add summary attributes

--- src/AdvancedBot.Core/Commands/Modules/PrefixModule.cs
+++ src/AdvancedBot.Core/Commands/Modules/PrefixModule.cs
@@ -18,6 +18,7 @@ namespace AdvancedBot.Core.Commands.Modules
         }
 
         [Command("add")]
+        [Summary("Adds the given prefix to this guild's prefixlist.")]
         public async Task AddPrefix([Remainder]string prefix)
         {
             var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
@@ -28,6 +29,7 @@ namespace AdvancedBot.Core.Commands.Modules
         }
 
         [Command("remove")]
+        [Summary("Removes the given prefix to the guild's prefixlist.")]
         public async Task RemovePrefix([Remainder]string prefix)
         {
             var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
@@ -38,6 +40,7 @@ namespace AdvancedBot.Core.Commands.Modules
         }
 
         [Command][Alias("list")]
+        [Summary("Lists the current prefixes.")]
         public async Task ListPrefix()
         {
             var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
