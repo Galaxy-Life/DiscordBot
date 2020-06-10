8d735c11f7fcb8c7ca9449f2df7e2aa908cfc753
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-06-10 17:33:58 +0200

    Clean up DI

--- src/AdvancedBot.Core/Commands/Modules/PrefixModule.cs
+++ src/AdvancedBot.Core/Commands/Modules/PrefixModule.cs
@@ -10,20 +10,13 @@ namespace AdvancedBot.Core.Commands.Modules
     [Group("prefix")][Alias("prefixes")]
     public class PrefixModule : CustomModule
     {
-        private GuildAccountService _accounts;
-
-        public PrefixModule(GuildAccountService accounts)
-        {
-            _accounts = accounts;
-        }
-
         [Command("add")]
         [Summary("Adds the given prefix to this guild's prefixlist.")]
         public async Task AddPrefix([Remainder]string prefix)
         {
-            var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
+            var guild = Accounts.GetOrCreateGuildAccount(Context.Guild.Id);
             guild.AddPrefix(prefix);
-            _accounts.SaveGuildAccount(guild);
+            Accounts.SaveGuildAccount(guild);
 
             await ReplyAsync($"Successfully added `{prefix}` to the current list.");
         }
@@ -32,9 +25,9 @@ namespace AdvancedBot.Core.Commands.Modules
         [Summary("Removes the given prefix to the guild's prefixlist.")]
         public async Task RemovePrefix([Remainder]string prefix)
         {
-            var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
+            var guild = Accounts.GetOrCreateGuildAccount(Context.Guild.Id);
             guild.RemovePrefix(prefix);
-            _accounts.SaveGuildAccount(guild);
+            Accounts.SaveGuildAccount(guild);
 
             await ReplyAsync($"Successfully removed `{prefix}` from the current list.");
         }
@@ -43,7 +36,7 @@ namespace AdvancedBot.Core.Commands.Modules
         [Summary("Lists the current prefixes.")]
         public async Task ListPrefix()
         {
-            var guild = _accounts.GetOrCreateGuildAccount(Context.Guild.Id);
+            var guild = Accounts.GetOrCreateGuildAccount(Context.Guild.Id);
 
             await ReplyAsync($"**Prefixes for {Context.Guild.Name}:**\n" +
                             $"`{string.Join("`, `", guild.Prefixes)}`");
