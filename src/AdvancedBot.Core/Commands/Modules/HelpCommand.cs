a428c44045a1e997f2b678fc5183553e02d62206
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-07-04 13:40:02 +0200

    Clean up

--- src/AdvancedBot.Core/Commands/Modules/HelpCommand.cs
+++ src/AdvancedBot.Core/Commands/Modules/HelpCommand.cs
@@ -4,15 +4,17 @@ using Discord.Commands;
 
 namespace AdvancedBot.Core.Commands.Modules
 {
+    [Name("info")]
+    [Summary("Category that handles all information regarding the bot.")]
     public class CommandInformationModule : TopModule
     {
         [Command("help")]
-        [Summary("Displays the help command.")]
+        [Summary("Displays information about the bot.")]
         public async Task Help()
             => await Commands.SendBotInfoAsync(Context);
 
         [Command("help")]
-        [Summary("Displays the help command for a specific command.")]
+        [Summary("Displays information about a specific command or category.")]
         public async Task Help([Remainder]string input)
         {
             var result = Commands.AdvancedSearch(input);
@@ -24,5 +26,15 @@ namespace AdvancedBot.Core.Commands.Modules
 
             await ReplyAsync("", false, embed.Build());
         }
+
+        [Command("commands")][Alias("cmds")][Priority(50)]
+        [Summary("Lists all commands the bot has.")]
+        public async Task DisplayAllCommands()
+            => await ReplyAsync(Commands.AllCommandsToString());
+
+        [Command("modules")][Alias("categories")]
+        [Summary("Lists all modules the bot has.")]
+        public async Task DisplayAllModules()
+            => await ReplyAsync(Commands.AllModulesToString());
     }
 }
