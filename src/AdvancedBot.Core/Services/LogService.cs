b6ec62b935b1b33e6cda85b646515e6fe4364cf3
Bryan <bryanpth@icloud.com>
2024-09-02 21:57:19 +0200

    refactor: update LogService.cs
    - Updated `GetEmbedForLog` returned embeds.
    - Renamed `victim` to `target`.
    - Formatting and identation changes for code readability.
    - Converted `GetColorBasedOnAction` switch statement to a switch expression for code readability.

--- src/AdvancedBot.Core/Services/LogService.cs
+++ src/AdvancedBot.Core/Services/LogService.cs
@@ -43,19 +43,19 @@ namespace AdvancedBot.Core.Services
             await channel.SendMessageAsync(embed: GetEmbedForLog(log, user));
         }
 
-        public Embed GetEmbedForLog(Log log, User victim)
+        public static Embed GetEmbedForLog(Log log, User target)
         {
             var embed = new EmbedBuilder()
-            {
-                Title = $"Case {log.Id} ({log.Type.Humanize()})",
-                Color = GetColorBasedOnAction(log.Type)
-            }
-            .AddField("Moderator", $"<@{log.DiscordModId}>", true);
+                .WithTitle($"Overview | {log.Type.Humanize()}")
+                .WithColor(GetColorBasedOnAction(log.Type))
+                .AddField("Moderator", $"<@{log.DiscordModId}>", true)
+                .WithFooter(footer => footer
+                    .WithText($"Case #{log.Id} • Requested by moderator with id {log.DiscordModId}"))
+                .WithCurrentTimestamp();
 
-            // if alliance action this can be null
-            if (victim != null)
+            if (target != null)
             {
-                embed.AddField("Victim", $"{victim.Name} ({victim.Id})", true);
+                embed.AddField("Target", $"{target.Name} (`{target.Id}`)", true);
             }
 
             switch (log.Type)
@@ -65,8 +65,79 @@ namespace AdvancedBot.Core.Services
                     {
                         embed.AddField("Reason", log.Reason, true);
                     }
-
                     break;
+
+                case LogAction.Ban:
+                    embed.AddField("Reason", log.Reason);
+                    embed.AddField("Expires on", log.Until == null ? "Never" : log.Until.Value.ToShortDateString());
+                    break;
+
+                case LogAction.GiveRole:
+                    embed.AddField("Role", log.Reason, true);
+                    break;
+
+                case LogAction.UpdateEmail:
+                case LogAction.UpdateName:
+                case LogAction.RenameAlliance:
+                    var splits = log.Reason.Split(':');
+                    embed.AddField("Previous", splits[0]);
+                    embed.AddField("Updated", splits[1], true);
+                    break;
+
+                case LogAction.AddChips:
+                    embed.AddField("Chips added", log.Reason);
+                    break;
+
+                case LogAction.AddItem:
+                    var itemSplit = log.Reason.Split(':');
+                    embed.AddField("Item SKU", itemSplit[0]);
+                    embed.AddField("Quantity", itemSplit[1]);
+                    break;
+
+                case LogAction.AddXp:
+                    embed.AddField("Experience added", log.Reason);
+                    break;
+
+                case LogAction.RemoveUserFromAlliance:
+                case LogAction.MakeUserAllianceOwner:
+                case LogAction.GetWarlogs:
+                    embed.AddField("Alliance", log.Reason, true);
+                    break;
+
+                case LogAction.ReloadRules:
+                case LogAction.GetTelemetry:
+                    embed.AddField("Server", log.Reason, true);
+                    break;
+
+                case LogAction.RunKicker:
+                case LogAction.ResetHelps:
+                case LogAction.Reset:
+                    embed.AddField("Server", log.Reason, true);
+                    break;
+
+                case LogAction.ForceWar:
+                case LogAction.ForceStopWar:
+                    var warSplit = log.Reason.Split(':');
+                    embed.AddField("Server", warSplit[0], true);
+                    embed.AddField("Alliance A", warSplit[1]);
+                    embed.AddField("Alliance B", warSplit[2]);
+                    break;
+
+                case LogAction.Compensate:
+                    var compensateSplit = log.Reason.Split(':');
+                    embed.AddField("Type", compensateSplit[0]);
+
+                    if (compensateSplit.Length > 2)
+                    {
+                        embed.AddField("Item SKU", compensateSplit[1]);
+                        embed.AddField("Quantity", compensateSplit[2]);
+                    }
+                    else
+                    {
+                        embed.AddField("Quantity", compensateSplit[1]);
+                    }
+                    break;
+
                 case LogAction.KickOffline:
                 case LogAction.AddBeta:
                 case LogAction.RemoveBeta:
@@ -79,67 +150,6 @@ namespace AdvancedBot.Core.Services
                 case LogAction.GetAccounts:
                 default:
                     break;
-                case LogAction.Ban:
-                    embed.AddField("Ban Reason", log.Reason, true);
-                    embed.AddField("Ends", log.Until == null ? "Never" : log.Until.Value.ToShortDateString());
-                    break;                
-                case LogAction.GiveRole:
-                    embed.AddField("Role", log.Reason, true);
-                    break;
-                case LogAction.UpdateEmail:
-                case LogAction.UpdateName:
-                case LogAction.RenameAlliance:
-                    var splits = log.Reason.Split(':');
-                    embed.AddField("From", splits[0]);
-                    embed.AddField("To", splits[1], true);
-                    break;
-                case LogAction.AddChips:
-                    embed.AddField("Chips added", log.Reason);
-                    break;
-                case LogAction.AddItem:
-                    var splitties = log.Reason.Split(':');
-                    embed.AddField("Item added", $"{splitties[1]}x item {splitties[0]}");
-                    break;
-                case LogAction.AddXp:
-                    embed.AddField("Xp added", log.Reason);
-                    break;
-                case LogAction.RemoveUserFromAlliance:
-                case LogAction.MakeUserAllianceOwner:
-                case LogAction.GetWarlogs:
-                    embed.AddField("Alliance", log.Reason, true);
-                    break;
-                case LogAction.ReloadRules:
-                    embed.AddField("Server", log.Reason, true);
-                    break;
-                case LogAction.GetTelemetry:
-                    embed.AddField("Type", log.Reason, true);
-                    break;
-                case LogAction.RunKicker:
-                case LogAction.ResetHelps:
-                case LogAction.Reset:
-                    embed.AddField("Server", log.Reason, true);
-                    break;
-                case LogAction.ForceWar:
-                case LogAction.ForceStopWar:
-                    var split = log.Reason.Split(':');
-                    embed.AddField("Server", split[0], true);
-                    embed.AddField("Alliance A", split[1]);
-                    embed.AddField("Alliance B", split[2]);
-                    break;
-                case LogAction.Compensate:
-                    var splitters = log.Reason.Split(':');
-                    embed.AddField("Type", splitters[0]);
-
-                    if (splitters.Length > 2)
-                    {
-                        embed.AddField("Sku", splitters[1]);
-                        embed.AddField("Amount", splitters[2]);
-                    }
-                    else 
-                    {
-                        embed.AddField("Amount", splitters[1]);
-                    }
-                    break;
             }
 
             return embed.Build();
@@ -147,33 +157,24 @@ namespace AdvancedBot.Core.Services
 
         private static Color GetColorBasedOnAction(LogAction action)
         {
-            switch (action)
+            return action switch
             {
-                case LogAction.Ban:
-                case LogAction.Reset:
-                case LogAction.KickOffline:
-                case LogAction.RemoveBeta:
-                case LogAction.RemoveUserFromAlliance:
-                case LogAction.EnableMaintenance:
-                case LogAction.ReloadRules:
-                    return Color.Red;
-                case LogAction.Unban:
-                case LogAction.AddBeta:
-                case LogAction.GiveRole:
-                    return Color.Green;
-                case LogAction.UpdateEmail:
-                case LogAction.UpdateName:
-                case LogAction.GetFull:
-                case LogAction.GetChipsBought:
-                case LogAction.MakeUserAllianceOwner:
-                case LogAction.GetWarlogs:
-                    return new Color(15710778);
-                case LogAction.AddChips:
-                case LogAction.AddItem:
-                case LogAction.RenameAlliance:
-                default:
-                    return Color.Blue;
-            }
+                LogAction.Ban or LogAction.Reset or LogAction.KickOffline or LogAction.RemoveBeta or 
+                LogAction.RemoveUserFromAlliance or LogAction.EnableMaintenance or LogAction.ReloadRules
+                    => Color.Red,
+
+                LogAction.Unban or LogAction.AddBeta or LogAction.GiveRole
+                    => Color.Green,
+
+                LogAction.UpdateEmail or LogAction.UpdateName or LogAction.GetFull or LogAction.GetChipsBought or
+                LogAction.MakeUserAllianceOwner or LogAction.GetWarlogs
+                    => new Color(15710778),
+
+                LogAction.AddChips or LogAction.AddItem or LogAction.RenameAlliance
+                    => Color.Blue,
+
+                _ => Color.Blue
+            };
         }
     }
 }
