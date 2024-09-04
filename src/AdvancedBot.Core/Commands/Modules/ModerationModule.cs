21568f06a19056fa3b0b972cfb4be79be1f42937
svr333 <vanrompaey.senne@protonmail.com>
2024-09-05 00:30:03 +0200

    Update src/AdvancedBot.Core/Commands/Modules/ModerationModule.cs

--- src/AdvancedBot.Core/Commands/Modules/ModerationModule.cs
+++ src/AdvancedBot.Core/Commands/Modules/ModerationModule.cs
@@ -485,7 +485,7 @@ namespace AdvancedBot.Core.Commands.Modules
         [SlashCommand("compensatechips", "Compensate chips to all users")]
         public async Task CompensateChips(uint amount)
         {
-            _ = GLClient.Production.CompensateChips(amount);
+            await GLClient.Production.CompensateChips(amount);
 
             await LogService.LogGameActionAsync(LogAction.Compensate, Context.User.Id, 0, $"Chips:{amount}");
 
