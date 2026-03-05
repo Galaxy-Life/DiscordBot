fd1435e0bae1f6371ba515ee1eed27c8a2c8021d
svr333 <vanrompaey.senne@protonmail.com>
2026-03-05 20:19:41 +0100

    Use default client

--- src/AdvancedBot.Core/Services/PhoenixWrapperService.cs
+++ src/AdvancedBot.Core/Services/PhoenixWrapperService.cs
@@ -8,16 +8,49 @@ namespace AdvancedBot.Core.Services;
 public class PhoenixWrapperService
 {
     private PhoenixCredentials _credentials;
+    private ApiClient _defaultClient;
+
+    private readonly List<ulong> _powerUsers =
+    [
+        202095042372829184, // svr333
+        942849642931032164, // lifecoder
+        180676108088246272, // lodethebig
+        356060824223350784, // andyvv.
+        275698828974489612, // magniolya
+        424689465450037278  // bryan
+    ];
 
     public PhoenixWrapperService(PhoenixCredentials credentials)
     {
         _credentials = credentials;
+
+        var client = new PhoenixClients(
+            new HttpClient() { Timeout = TimeSpan.FromSeconds(10) },
+            new PhoenixApiClientOptions
+            {
+                TokenEndpoint = new Uri("https://accounts.phoenixnetwork.net/api/oauth/token"),
+                ClientId = _credentials.ClientId,
+                ClientSecret = _credentials.ClientSecret,
+                Scopes = [],
+            }).PhoenixClient;
+
+        if (client == null)
+        {
+            throw new Exception("Failed to create default profile");
+        }
+
+        _defaultClient = client;
     }
 
     public Dictionary<ulong, ApiClient> PhoenixClients { get; set; } = [];
 
     public ApiClient GetClient(ulong discordId)
     {
+        if (!_powerUsers.Contains(discordId))
+        {
+            return _defaultClient;
+        }
+
         if (PhoenixClients.ContainsKey(discordId))
         {
             return PhoenixClients[discordId];
